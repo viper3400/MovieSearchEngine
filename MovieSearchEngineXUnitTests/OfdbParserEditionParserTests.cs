f2c092a8147273c09a63a6944fa1f192fbc9e22a
viper3400 <viper_3400@gmx.de>
2019-06-29 11:01:26 +0200

    Replaced UnitTests with XUnit

--- MovieSearchEngineXUnitTests/OfdbParserEditionParserTests.cs
+++ MovieSearchEngineXUnitTests/OfdbParserEditionParserTests.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MovieSearchEngineUnitTests
+namespace MovieSearchXEngineUnitTests
 {
-    [TestClass]
-    public class OfdbParserEditionParserTest
+    public class OfdbParserEditionParserTests
     {
-        private List<string> input 
-        { 
-            get 
+        private List<string> input
+        {
+            get
             {
                 var s = new List<string>();
 
@@ -24,7 +21,7 @@ namespace MovieSearchEngineUnitTests
                 s.Add("<tr valign=\"top\"><td nowrap=\"\"><font face=\"Arial,Helvetica,sans-serif\" size=\"2\" class=\"Normal\">USA:&nbsp;&nbsp;</font></td><td nowrap=\"\">&nbsp;</td><td><font face=\"Arial,Helvetica,sans-serif\" size=\"2\" class=\"Daten\"><b><a href=\"view.php?page=fassung&fid=258134&vid=390402\" onmouseover=\"Tip('&lt;img src=&quot;images/fassung/390/390402_f.jpg&quot; width=&quot;220&quot; height=&quot;276&quot;&gt;',SHADOW,true)\">Blu-ray Disc: Summit (Steelbook) (BD & DVD) (Best Buy exkl.)</a></b><font class=\"Klein\"></font></font></td></tr>");
 
                 return s;
-            } 
+            }
         }
 
         private List<MovieMetaEngine.MovieMetaEditionModel> expectedEditionModel
@@ -34,16 +31,16 @@ namespace MovieSearchEngineUnitTests
                 var expected = new List<MovieMetaEngine.MovieMetaEditionModel>();
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
-                    {
-                        MetaEngine = "OFDB",
-                        Name = "DVD: Concorde",
-                        Country = "Deutschland",
-                        Reference = "258134;385676"
-                    });
+                {
+                    MetaEngine = "ofdb",
+                    Name = "DVD: Concorde",
+                    Country = "Deutschland",
+                    Reference = "258134;385676"
+                });
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
                 {
-                    MetaEngine = "OFDB",
+                    MetaEngine = "ofdb",
                     Name = "Blu-ray Disc: Concorde (Deluxe Fan Edition)",
                     Country = "Deutschland",
                     Reference = "258134;386665"
@@ -51,7 +48,7 @@ namespace MovieSearchEngineUnitTests
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
                 {
-                    MetaEngine = "OFDB",
+                    MetaEngine = "ofdb",
                     Name = "Blu-ray Disc: Concorde (Deluxe Fan Edition) (ohne Schuber)",
                     Country = "Deutschland",
                     Reference = "258134;386666"
@@ -59,7 +56,7 @@ namespace MovieSearchEngineUnitTests
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
                 {
-                    MetaEngine = "OFDB",
+                    MetaEngine = "ofdb",
                     Name = "Kino: Concorde Filmverleih",
                     Country = "Deutschland",
                     Reference = "258134;379177"
@@ -67,7 +64,7 @@ namespace MovieSearchEngineUnitTests
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
                 {
-                    MetaEngine = "OFDB",
+                    MetaEngine = "ofdb",
                     Name = "Pay-TV: sky Cinema",
                     Country = "Deutschland",
                     Reference = "258134;396134"
@@ -75,29 +72,31 @@ namespace MovieSearchEngineUnitTests
 
                 expected.Add(new MovieMetaEngine.MovieMetaEditionModel()
                 {
-                    MetaEngine = "OFDB",
+                    MetaEngine = "ofdb",
                     Name = ">Blu-ray Disc: Summit (Steelbook) (BD & DVD) (Best Buy exkl.)",
                     Country = "Deutschland",
                     Reference = "258134;390402"
                 });
-                
-                
+
+
                 return expected;
             }
         }
 
-        [TestCategory("CAT_OFFLINE")]
-        [TestMethod]
+        [Trait("Category","CAT_OFFLINE")]
+        [Fact]
         public void OfdbDetailsEdtionsResolverTest()
         {
             var SUT = new OfdbParser.OfdbResolvers.OfdbDetailsEdtionsResolver();
             var actual = SUT.Resolve<List<MovieMetaEngine.MovieMetaEditionModel>>(input);
-            
-            
-            Assert.Inconclusive();
 
+            var expected = expectedEditionModel.FirstOrDefault(e => e.Reference == "258134;396134");
+            var actualSUT = actual.FirstOrDefault(e => e.Reference == "258134;396134");
+            Assert.Equal(expected.MetaEngine, actualSUT.MetaEngine);
+            Assert.Equal(expected.Country, actualSUT.Country);
+            Assert.Equal(expected.Name, actualSUT.Name);
         }
     }
 
-    
+
 }
